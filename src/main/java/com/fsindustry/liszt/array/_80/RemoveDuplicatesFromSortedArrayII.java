f788f644b975f137a24b0657893bd97b0238f0da
fsindustry <294127378@qq.com>
2022-06-23 22:53:59 +0800

    刷题

--- src/main/java/com/fsindustry/liszt/array/_80/RemoveDuplicatesFromSortedArrayII.java
+++ src/main/java/com/fsindustry/liszt/array/_80/RemoveDuplicatesFromSortedArrayII.java
@@ -1,11 +1,11 @@
-package com.fsindustry.liszt.array._26;
+package com.fsindustry.liszt.array._80;
 
 import java.util.Arrays;
 
 /**
- * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
  */
-public class RemoveDuplicatesFromSortedArray {
+public class RemoveDuplicatesFromSortedArrayII {
 
     public static void main(String[] args) {
         test1();
@@ -19,14 +19,14 @@ public class RemoveDuplicatesFromSortedArray {
 
     public static void test1() {
         Solution solution = new Solution();
-        int[] nums = {1, 1, 2};
+        int[] nums = {1, 1, 1, 2, 2, 3};
         System.out.println("Input: " + Arrays.toString(nums));
         System.out.println("Output: " + solution.removeDuplicates(nums) + ", arr: " + Arrays.toString(nums));
     }
 
     public static void test2() {
         Solution solution = new Solution();
-        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
         System.out.println("Input: " + Arrays.toString(nums));
         System.out.println("Output: " + solution.removeDuplicates(nums) + ", arr: " + Arrays.toString(nums));
     }
@@ -34,14 +34,20 @@ public class RemoveDuplicatesFromSortedArray {
 
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int count = 0;
         int k = 0; // [0,k) 存放去重后的k个元素；
         // 循环不变量：[0,k)区间为去重后元素区间，随k值增长而扩大；
         // 遍历nums，将其中元素放入[0,k)区间
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[k] != nums[i]) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[k] && count < 3) {
+                count++;
                 k++;
-                if (k != i) swap(nums, k, i);
+            } else {
+                swap(nums, k, i);
+                k++;
+                count = 0;
             }
+
         }
 
         return k + 1;
