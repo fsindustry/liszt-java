759da625e5c93cd3693c7481c7f68ca3aeae4a48
fsindustry <294127378@qq.com>
2022-07-13 22:03:31 +0800

    刷题：查找表+滑动窗口问题

--- src/main/java/com/fsindustry/liszt/lut4/_220/ContainsDuplicateIII.java
+++ src/main/java/com/fsindustry/liszt/lut4/_220/ContainsDuplicateIII.java
@@ -21,6 +21,7 @@ class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         TreeSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
+            // 若存在一个ceiling，使得 nums[i] -t <= ceiling <= nums[i] + t，则满足条件：abs(nums[i] - nums[j]) <= t
             Long ceiling = set.ceiling((long) nums[i] - t);
             if (ceiling != null && ceiling <= (long) nums[i] + t) {
                 return true;
@@ -28,6 +29,7 @@ class Solution {
                 set.add((long) nums[i]);
             }
 
+            // 若i，j之间长度超过k，则缩小窗口
             if (set.size() == k + 1) {
                 set.remove((long) nums[i - k]);
             }
