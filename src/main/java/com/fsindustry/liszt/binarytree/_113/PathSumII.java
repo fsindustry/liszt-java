da99255df8197924a476c2ffe485b63a03ffea9a
FUZHENGXIN <fuzhengxin@kingsoft.com>
2022-09-08 23:19:53 +0800

    Changes

--- src/main/java/com/fsindustry/liszt/binarytree/_113/PathSumII.java
+++ src/main/java/com/fsindustry/liszt/binarytree/_113/PathSumII.java
@@ -1,5 +1,7 @@
 package com.fsindustry.liszt.binarytree._113;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PathSumII {
@@ -26,6 +28,33 @@ class TreeNode {
 
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        return null;
+        if (null == root) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(root, targetSum, result, path);
+        return result;
+    }
+
+    public void dfs(TreeNode root, int targetSum, List<List<Integer>> result, List<Integer> path) {
+        if(null == root){
+            return;
+        }
+
+        if (isLeaf(root) && targetSum == root.val) {
+            List<Integer> res = new ArrayList<>(path);
+            res.add(root.val);
+            result.add(res);
+        }
+
+        path.add(root.val);
+        dfs(root.left, targetSum - root.val, result, path);
+        dfs(root.right, targetSum - root.val, result, path);
+        path.remove(path.size() - 1);
+    }
+
+    boolean isLeaf(TreeNode node) {
+        return null == node.left && null == node.right;
     }
 }
