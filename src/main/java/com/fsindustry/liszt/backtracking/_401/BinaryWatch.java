2aa23bc83b9d10c868e76180b4c3f14402dbd024
FUZHENGXIN <fuzhengxin@kingsoft.com>
2022-10-03 23:40:17 +0800

    committed by fsindustry@2022/10/03 about: combinations

---
+++ src/main/java/com/fsindustry/liszt/backtracking/_401/BinaryWatch.java
@@ -0,0 +1,21 @@
+package com.fsindustry.liszt.backtracking._401;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryWatch {
+}
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> res = new ArrayList<>();
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h << 6 | m) == turnedOn) {
+                    res.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        return res;
+    }
+}
