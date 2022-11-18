c92ae6fbc9dd4f595b0d8f985eb1462acb62cf3e
fsindustry <294127378@qq.com>
2022-11-18 10:49:26 +0800

    刷题：DP 股票买卖问题

---
+++ src/main/java/com/fsindustry/liszt/dp3_stock/_122/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,40 @@
+package com.fsindustry.liszt.dp3_stock._122;
+
+public class BestTimeToBuyAndSellStockII {
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (null == prices || 0 == prices.length) {
+            return 0;
+        }
+        int n = prices.length;
+        int i0 = 0, i1 = -prices[0];
+        for (int i = 0; i < n; i++) {
+            i0 = Math.max(i0, i1 + prices[i]);
+            i1 = Math.max(i1, i0 - prices[i]);
+        }
+        return i0;
+    }
+}
+
+class Solution1 {
+    public int maxProfit(int[] prices) {
+        if (null == prices || 0 == prices.length) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] cache = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (0 == i) {
+                cache[0][0] = 0;
+                cache[0][1] = -prices[0];
+                continue;
+            }
+
+            cache[i][0] = Math.max(cache[i - 1][0], cache[i - 1][1] + prices[i]);
+            cache[i][1] = Math.max(cache[i - 1][1], cache[i - 1][0] - prices[i]);
+        }
+        return cache[n - 1][0];
+    }
+}
